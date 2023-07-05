5b6b3da341c7aa34a3e134620ff288919e38d2df
RIKUTO_SHIN <91378501+rikuto1231@users.noreply.github.com>
2023-07-05 21:49:03 +0900

    Update book-out.php

--- book-out.php
+++ book-out.php
@@ -7,91 +7,92 @@
 </head>
 <body>
     <h1>商品一覧</h1>
-    <table border="1">
-        <tr>
-            <th>商品名</th>
-        </tr>
 
-        <?php
-            $host = '***'; // データベースサーバーのホスト名
-            $dbname = '***'; // 使用するデータベース名
-            $user = '***'; // データベースのユーザー名
-            $password = '***'; // データベースのパスワード
-            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    <?php
+class Main {
+    
+    private $host = '***'; // データベースサーバーのホスト名
+    private $dbname = '***'; // 使用するデータベース名
+    private $user = '***'; // データベースのユーザー名
+    private $password = '***'; // データベースのパスワード
+    private $pdo; 
 
-            $array_category = array(
-                '0' => "None",
-                '1' => "プログラミング",
-                '2' => "青春小説",
-                '3' => "恋愛小説"
-            );
+    private $array_category = array(
+        '0' => "None",
+        '1' => "プログラミング",
+        '2' => "青春小説",
+        '3' => "恋愛小説"
+    );
 
-            $array_price1 = array(
-                '0' => 0,
-                '1' => 500,
-                '2' => 1000,
-                '3' => 2000,
-                '4' => 3000,
-                '5' => 5000,
-            );
+    private $array_price1 = array(
+        '0' => 0,
+        '1' => 500,
+        '2' => 1000,
+        '3' => 2000,
+        '4' => 3000,
+        '5' => 5000,
+    );
 
-            $array_price2 = array(
-                '0' => 0,
-                '1' => 0,
-                '2' => 501,
-                '3' => 1001,
-                '4' => 2001,
-                '5' => 3001,
-            );
+    private $array_price2 = array(
+        '0' => 0,
+        '1' => 0,
+        '2' => 501,
+        '3' => 1001,
+        '4' => 2001,
+        '5' => 3001,
+    );
 
-            $search = $_POST['search_text'];
-            $category = $_POST['category'];
-            $category_name = $array_category[$category];
-            $price = $_POST['price'];
-            $price1 = $array_price1[$price];
-            $price2 = $array_price2[$price];
+    public function __construct() {
+        $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8", $this->user, $this->password);
+    }
 
-            if ($category == 0 and $price == 0) {
-                $sql = $pdo->prepare('select * from Books where book_name like ?');
-                $sql->execute(["%$search%"]);
+    public function displayBooks() {
+        $search = $_POST['search_text'] ?? '';
+        $category = $_POST['category'] ?? 0;
+        $category_name = $this->array_category[$category];
+        $price = $_POST['price'] ?? 0;
+        $price1 = $this->array_price1[$price];
+        $price2 = $this->array_price2[$price];
 
-                
-            } else if ($category != 0 and $price != 0) {
-                $sql = $pdo->prepare('select * from Books where book_name like ? and category =? and price between ? and ?');
-                $sql->execute(["%$search%", $category_name, $price2, $price1]);
+        if ($category == 0 && $price == 0) {
+            $sql = $this->pdo->prepare('select * from Books where book_name like :search order by book_id asc');
+            $sql->bindValue(':search', "%$search%");
+            $sql->execute();
+        } else if ($category != 0 && $price != 0) {
+            $sql = $this->pdo->prepare('select * from Books where book_name like :search and category = :category and price between :price2 and :price1 order by book_id asc');
+            $sql->bindValue(':search', "%$search%");
+            $sql->bindValue(':category', $category_name);
+            $sql->bindValue(':price2', $price2);
+            $sql->bindValue(':price1', $price1);
+            $sql->execute();
+        } else if ($category != 0 && $price == 0) {
+            $sql = $this->pdo->prepare('select * from Books where book_name like :search and category = :category order by book_id asc');
+            $sql->bindValue(':search', "%$search%");
+            $sql->bindValue(':category', $category_name);
+            $sql->execute();
+        } else {
+            $sql = $this->pdo->prepare('select * from Books where book_name like :search and price between :price2 and :price1 order by book_id asc');
+            $sql->bindValue(':search', "%$search%");
+            $sql->bindValue(':price2', $price2);
+            $sql->bindValue(':price1', $price1);
+            $sql->execute();
+        }
 
-                
-            } else if ($category != 0 and $price == 0) {
-                $sql = $pdo->prepare('select * from Books where book_name like ? and category =?');
-                $sql->execute(["%$search%", $category_name]);
-
-                
-            } else {
-                $sql = $pdo->prepare('select * from Books where book_name like ? and price between ? and ?');
-                $sql->execute(["%$search%", $price2, $price1]);
-
-                
-            }
-            foreach ($sql as $row) {
-                echo '<tr>';
-                echo '<td>', $row['book_name'], '</td>';
-                echo '</tr>';
-            }
-
-            echo '<p><button type="button" class="delete" onclick="history.back()">戻る</button></p>';
-
-
-            //試作品枠
-            //要素を出して後でcss、db調整
+        foreach ($sql as $row) {
             echo '<div class="container">';
             echo '<div class="set1">';
-            echo '<p class="syohin_id">商品ID</p>';
-            echo '<img class="syohin_img" src="img/book_db.jpg">';
+            echo '<p class="syohin_id">', $row['book_id'], '</p>';
+            if(isset($row['book_img'])){
+                echo '<img class="syohin_img" src="img/', $row['book_img'], '">';
+            }else{
+                echo '<img class="syohin_img" src="img/book_null.jpg">';
+            }
+            
             echo '</div>';
             echo '<div class="set2">';
-            echo '<h4 class="syohin_name">商品名</h4>';
-            echo '<h4 class="syohin_price">商品単価</h4>';
-            echo '<p class="reg_date">商品登録日</p>';
+            echo '<h4 class="syohin_name">', $row['book_name'], '</h4>';
+            echo '<h4 class="syohin_price">', $row['price'], '</h4>';
+            echo '<p class="reg_date">', $row['book_detail'], '</p>';
             echo '<p class="up_date">商品更新日</p>';
             echo '<p class="syohin_exp">商品説明</p>';
             echo '</div>';
@@ -100,7 +101,18 @@
             echo '<p><button type="submit" class="delete">削除</button></p>';
             echo '</div>';
             echo '</div>';
-        ?>
-    </table>
+        }
+
+        echo '<p><button type="button" class="delete" onclick="history.back()">戻る</button></p>';
+    }
+}
+
+// Mainのインスタンス作成
+$main = new Main();
+// displayBooksメソッドを呼び出して表示
+$main->displayBooks();
+?>
+
+
 </body>
 </html>
